a402873c588b9dbfd11a75cf57f34e6a918aa5fb
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-21 11:29:18 +0300

    build: add tsup build system
    Modernize build system and package configuration
    
    - Migrate from webpack to tsup for improved build process.
    - Update package.json with new exports configuration and dependencies.
    - Add tsup.config.ts for flexible bundling options.
    - Target both cjs and esm
    - Consolidate types with bundle
    - Use minified bundled code for both browser and node

--- src/Typesense/Configuration.ts
+++ src/Typesense/Configuration.ts
@@ -1,35 +1,35 @@
-import * as logger from "loglevel";
-import { Logger, LogLevelDesc } from "loglevel";
-import { MissingConfigurationError } from "./Errors";
-import type { Agent as HTTPAgent } from "http";
-import type { Agent as HTTPSAgent } from "https";
+import * as logger from "loglevel"
+import { Logger, LogLevelDesc } from "loglevel"
+import { MissingConfigurationError } from "./Errors"
+import type { Agent as HTTPAgent } from "http"
+import type { Agent as HTTPSAgent } from "https"
 
 export interface NodeConfiguration {
-  host: string;
-  port: number;
-  protocol: string;
-  path?: string;
-  url?: string;
+  host: string
+  port: number
+  protocol: string
+  path?: string
+  url?: string
 }
 
 export interface NodeConfigurationWithHostname {
-  host: string;
-  port: number;
-  protocol: string;
-  path?: string;
+  host: string
+  port: number
+  protocol: string
+  path?: string
 }
 
 export interface NodeConfigurationWithUrl {
-  url: string;
+  url: string
 }
 
 export interface ConfigurationOptions {
-  apiKey: string;
+  apiKey: string
   nodes:
     | NodeConfiguration[]
     | NodeConfigurationWithHostname[]
-    | NodeConfigurationWithUrl[];
-  randomizeNodes?: boolean;
+    | NodeConfigurationWithUrl[]
+  randomizeNodes?: boolean
   /**
    * @deprecated
    * masterNode is now consolidated to nodes, starting with Typesense Server v0.12'
@@ -37,7 +37,7 @@ export interface ConfigurationOptions {
   masterNode?:
     | NodeConfiguration
     | NodeConfigurationWithHostname
-    | NodeConfigurationWithUrl;
+    | NodeConfigurationWithUrl
   /**
    * @deprecated
    * readReplicaNodes is now consolidated to nodes, starting with Typesense Server v0.12'
@@ -45,23 +45,23 @@ export interface ConfigurationOptions {
   readReplicaNodes?:
     | NodeConfiguration[]
     | NodeConfigurationWithHostname[]
-    | NodeConfigurationWithUrl[];
+    | NodeConfigurationWithUrl[]
   nearestNode?:
     | NodeConfiguration
     | NodeConfigurationWithHostname
-    | NodeConfigurationWithUrl;
-  connectionTimeoutSeconds?: number;
-  timeoutSeconds?: number;
-  healthcheckIntervalSeconds?: number;
-  numRetries?: number;
-  retryIntervalSeconds?: number;
-  sendApiKeyAsQueryParam?: boolean | undefined;
-  useServerSideSearchCache?: boolean;
-  cacheSearchResultsForSeconds?: number;
-  additionalHeaders?: Record<string, string>;
+    | NodeConfigurationWithUrl
+  connectionTimeoutSeconds?: number
+  timeoutSeconds?: number
+  healthcheckIntervalSeconds?: number
+  numRetries?: number
+  retryIntervalSeconds?: number
+  sendApiKeyAsQueryParam?: boolean | undefined
+  useServerSideSearchCache?: boolean
+  cacheSearchResultsForSeconds?: number
+  additionalHeaders?: Record<string, string>
 
-  logLevel?: LogLevelDesc;
-  logger?: Logger;
+  logLevel?: LogLevelDesc
+  logger?: Logger
 
   /**
    * Set a custom HTTP Agent
@@ -73,7 +73,7 @@ export interface ConfigurationOptions {
    *      httpAgent: new HTTPAgent({ keepAlive: true }),
    * @type {HTTPAgent}
    */
-  httpAgent?: HTTPAgent;
+  httpAgent?: HTTPAgent
 
   /**
    * Set a custom HTTPS Agent
@@ -85,7 +85,7 @@ export interface ConfigurationOptions {
    *      httpsAgent: new HTTPSAgent({ keepAlive: true }),
    * @type {HTTPSAgent}
    */
-  httpsAgent?: HTTPSAgent;
+  httpsAgent?: HTTPSAgent
 
   /**
    * Set a custom paramsSerializer
@@ -94,88 +94,88 @@ export interface ConfigurationOptions {
    *  This is helpful for handling React Native issues like this: https://github.com/axios/axios/issues/6102#issuecomment-2085301397
    * @type {any}
    */
-  paramsSerializer?: any;
+  paramsSerializer?: any
 }
 
 export default class Configuration {
   readonly nodes:
     | NodeConfiguration[]
     | NodeConfigurationWithHostname[]
-    | NodeConfigurationWithUrl[];
+    | NodeConfigurationWithUrl[]
   readonly nearestNode?:
     | NodeConfiguration
     | NodeConfigurationWithHostname
-    | NodeConfigurationWithUrl;
-  readonly connectionTimeoutSeconds: number;
-  readonly healthcheckIntervalSeconds: number;
-  readonly numRetries: number;
-  readonly retryIntervalSeconds: number;
-  readonly apiKey: string;
-  readonly sendApiKeyAsQueryParam?: boolean;
-  readonly cacheSearchResultsForSeconds: number;
-  readonly useServerSideSearchCache: boolean;
-  readonly logger: Logger;
-  readonly logLevel: LogLevelDesc;
-  readonly additionalHeaders?: Record<string, string>;
-  readonly httpAgent?: HTTPAgent;
-  readonly httpsAgent?: HTTPSAgent;
-  readonly paramsSerializer?: any;
+    | NodeConfigurationWithUrl
+  readonly connectionTimeoutSeconds: number
+  readonly healthcheckIntervalSeconds: number
+  readonly numRetries: number
+  readonly retryIntervalSeconds: number
+  readonly apiKey: string
+  readonly sendApiKeyAsQueryParam?: boolean
+  readonly cacheSearchResultsForSeconds: number
+  readonly useServerSideSearchCache: boolean
+  readonly logger: Logger
+  readonly logLevel: LogLevelDesc
+  readonly additionalHeaders?: Record<string, string>
+  readonly httpAgent?: HTTPAgent
+  readonly httpsAgent?: HTTPSAgent
+  readonly paramsSerializer?: any
 
   constructor(options: ConfigurationOptions) {
-    this.nodes = options.nodes || [];
+    this.nodes = options.nodes || []
     this.nodes = this.nodes
       .map((node) => this.setDefaultPathInNode(node))
       .map((node) => this.setDefaultPortInNode(node))
-      .map((node) => ({ ...node })) as NodeConfiguration[]; // Make a deep copy
+      .map((node) => ({ ...node })) as NodeConfiguration[] // Make a deep copy
 
     if (options.randomizeNodes == null) {
-      options.randomizeNodes = true;
+      options.randomizeNodes = true
     }
 
     if (options.randomizeNodes === true) {
-      this.shuffleArray(this.nodes);
+      this.shuffleArray(this.nodes)
     }
 
-    this.nearestNode = options.nearestNode;
-    this.nearestNode = this.setDefaultPathInNode(this.nearestNode);
-    this.nearestNode = this.setDefaultPortInNode(this.nearestNode);
+    this.nearestNode = options.nearestNode
+    this.nearestNode = this.setDefaultPathInNode(this.nearestNode)
+    this.nearestNode = this.setDefaultPortInNode(this.nearestNode)
 
     this.connectionTimeoutSeconds =
-      options.connectionTimeoutSeconds || options.timeoutSeconds || 5;
-    this.healthcheckIntervalSeconds = options.healthcheckIntervalSeconds || 60;
+      options.connectionTimeoutSeconds || options.timeoutSeconds || 5
+    this.healthcheckIntervalSeconds = options.healthcheckIntervalSeconds || 60
     this.numRetries =
       options.numRetries ||
       this.nodes.length + (this.nearestNode == null ? 0 : 1) ||
-      3;
-    this.retryIntervalSeconds = options.retryIntervalSeconds || 0.1;
+      3
+    this.retryIntervalSeconds = options.retryIntervalSeconds || 0.1
 
-    this.apiKey = options.apiKey;
-    this.sendApiKeyAsQueryParam = options.sendApiKeyAsQueryParam; // We will set a default for this in Client and SearchClient
+    this.apiKey = options.apiKey
+    this.sendApiKeyAsQueryParam = options.sendApiKeyAsQueryParam // We will set a default for this in Client and SearchClient
 
     this.cacheSearchResultsForSeconds =
-      options.cacheSearchResultsForSeconds || 0; // Disable client-side cache by default
-    this.useServerSideSearchCache = options.useServerSideSearchCache || false;
+      options.cacheSearchResultsForSeconds || 0 // Disable client-side cache by default
+    this.useServerSideSearchCache = options.useServerSideSearchCache || false
 
-    this.logger = options.logger || logger;
-    this.logLevel = options.logLevel || "warn";
-    this.logger.setLevel(this.logLevel);
+    this.logger = options.logger || logger
+    this.logLevel = options.logLevel || "warn"
+    this.logger.setLevel(this.logLevel)
 
-    this.additionalHeaders = options.additionalHeaders;
+    this.additionalHeaders = options.additionalHeaders
 
-    this.httpAgent = options.httpAgent;
-    this.httpsAgent = options.httpsAgent;
+    this.httpAgent = options.httpAgent
+    this.httpsAgent = options.httpsAgent
 
-    this.paramsSerializer = options.paramsSerializer;
+    this.paramsSerializer = options.paramsSerializer
 
-    this.showDeprecationWarnings(options);
-    this.validate();
+    this.showDeprecationWarnings(options)
+    this.validate()
   }
 
   validate(): boolean {
     if (this.nodes == null || this.nodes.length === 0 || this.validateNodes()) {
       throw new MissingConfigurationError(
-        "Ensure that nodes[].protocol, nodes[].host and nodes[].port are set",
-      );
+        "Ensure that nodes[].protocol, nodes[].host and nodes[].port are set"
+      )
     }
 
     if (
@@ -183,34 +183,34 @@ export default class Configuration {
       this.isNodeMissingAnyParameters(this.nearestNode)
     ) {
       throw new MissingConfigurationError(
-        "Ensure that nearestNodes.protocol, nearestNodes.host and nearestNodes.port are set",
-      );
+        "Ensure that nearestNodes.protocol, nearestNodes.host and nearestNodes.port are set"
+      )
     }
 
     if (this.apiKey == null) {
-      throw new MissingConfigurationError("Ensure that apiKey is set");
+      throw new MissingConfigurationError("Ensure that apiKey is set")
     }
 
-    return true;
+    return true
   }
 
   private validateNodes(): boolean {
     return this.nodes.some((node) => {
-      return this.isNodeMissingAnyParameters(node);
-    });
+      return this.isNodeMissingAnyParameters(node)
+    })
   }
 
   private isNodeMissingAnyParameters(
     node:
       | NodeConfiguration
       | NodeConfigurationWithHostname
-      | NodeConfigurationWithUrl,
+      | NodeConfigurationWithUrl
   ): boolean {
     return (
       !["protocol", "host", "port", "path"].every((key) => {
-        return node.hasOwnProperty(key);
+        return node.hasOwnProperty(key)
       }) && node["url"] == null
-    );
+    )
   }
 
   private setDefaultPathInNode(
@@ -218,16 +218,16 @@ export default class Configuration {
       | NodeConfiguration
       | NodeConfigurationWithHostname
       | NodeConfigurationWithUrl
-      | undefined,
+      | undefined
   ):
     | NodeConfiguration
     | NodeConfigurationWithHostname
     | NodeConfigurationWithUrl
     | undefined {
     if (node != null && !node.hasOwnProperty("path")) {
-      node["path"] = "";
+      node["path"] = ""
     }
-    return node;
+    return node
   }
 
   private setDefaultPortInNode(
@@ -235,7 +235,7 @@ export default class Configuration {
       | NodeConfiguration
       | NodeConfigurationWithHostname
       | NodeConfigurationWithUrl
-      | undefined,
+      | undefined
   ):
     | NodeConfiguration
     | NodeConfigurationWithHostname
@@ -248,38 +248,38 @@ export default class Configuration {
     ) {
       switch (node["protocol"]) {
         case "https":
-          node["port"] = 443;
-          break;
+          node["port"] = 443
+          break
         case "http":
-          node["port"] = 80;
-          break;
+          node["port"] = 80
+          break
       }
     }
-    return node;
+    return node
   }
 
   private showDeprecationWarnings(options: ConfigurationOptions): void {
     if (options.timeoutSeconds) {
       this.logger.warn(
-        "Deprecation warning: timeoutSeconds is now renamed to connectionTimeoutSeconds",
-      );
+        "Deprecation warning: timeoutSeconds is now renamed to connectionTimeoutSeconds"
+      )
     }
     if (options.masterNode) {
       this.logger.warn(
-        "Deprecation warning: masterNode is now consolidated to nodes, starting with Typesense Server v0.12",
-      );
+        "Deprecation warning: masterNode is now consolidated to nodes, starting with Typesense Server v0.12"
+      )
     }
     if (options.readReplicaNodes) {
       this.logger.warn(
-        "Deprecation warning: readReplicaNodes is now consolidated to nodes, starting with Typesense Server v0.12",
-      );
+        "Deprecation warning: readReplicaNodes is now consolidated to nodes, starting with Typesense Server v0.12"
+      )
     }
   }
 
   private shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j], array[i]]
     }
   }
 }
